1ffe819b41c934d9f3a1a9bc55cd6a947ef3c2aa
Billy <buiviettru@gmail.com>
2021-01-07 22:46:34 +0700

    debug 8

--- main.tf
+++ main.tf
@@ -25,13 +25,22 @@ provider "random" {}
 
 resource "random_pet" "sg" {}
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "ubuntu"
+  public_key = file("key.pub")
+}
+
 resource "aws_instance" "web" {
+  key_name               = aws_key_pair.deployer.key_name
   ami                    = "ami-06fb5332e8e3e577a"
   instance_type          = "t2.small"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  
 
+  user_data = <<-EOF
+  git clone https://github.com/billybui/test-parcel-app.git && chmod +x test-parcel-app/startup.sh && test-parcel-app/startup.sh &
+              EOF
 }
+
 
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
