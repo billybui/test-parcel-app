c6cd7885b8ab3f5e0281b2e57e97d8cfd18ec483
Billy <buiviettru@gmail.com>
2021-01-07 16:06:32 +0700

    Remove key nadme

--- main.tf
+++ main.tf
@@ -29,7 +29,6 @@ resource "aws_instance" "web" {
   ami                    = "ami-06fb5332e8e3e577a"
   instance_type          = "t2.small"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  key_name = "tru"
 
   provisioner "file" {
     source      = "schema.sql"
